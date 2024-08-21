d10231e3220f0a9089f1a0c18faea5114d8cecc3
ArthurMello <104700273+ArthurDMello@users.noreply.github.com>
2024-08-20 22:19:20 -0300

    refactor backend

--- cars/cars/src/main/java/com/cars/cars/repositories/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cars.cars.repositories;
-
-import com.cars.cars.model.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
